077d7ba53dd0a91fe20fe9c1fff3e7c021f82e4e
Enrico Mazzucchelli <enrico.mazzucchelli@fabrick.com>
2022-12-27 23:26:37 +0100

    Minor

--- app/src/main/java/com/mzzlab/sample/contactsapp/MainActivity.kt
+++ app/src/main/java/com/mzzlab/sample/contactsapp/MainActivity.kt
@@ -10,34 +10,14 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.mzzlab.sample.contactsapp.ui.App
 import com.mzzlab.sample.contactsapp.ui.theme.ContactsAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ContactsAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            App()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ContactsAppTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/mzzlab/sample/contactsapp/ui/App.kt
@@ -0,0 +1,12 @@
+package com.mzzlab.sample.contactsapp.ui
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import com.mzzlab.sample.contactsapp.ui.theme.ContactsAppTheme
+
+@Composable
+fun App(){
+    ContactsAppTheme {
+        Text(text = "Hello !!!")
+    }
+}
